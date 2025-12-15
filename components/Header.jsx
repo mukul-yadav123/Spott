114856b56b513c070f16529d6f768b8e631e279d
Mukul Yadav <mukuly2002@gmail.com>
2025-12-15 11:35:07 +0530

    Header

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+          <Link href={"/"} className="flex items-center">
+            <Image
+              src="/spott.png"
+              alt="Spott Logo"
+              width={100}
+              height={100}
+              classname="w-full h-11"
+              priority
+            />
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
